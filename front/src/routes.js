85e950b17dd720febc9c65ac28a55ca1de533a2f
Luana <dev@orions.com.br>
2020-05-25 18:37:27 -0300

    Relatório Produtos com defeito

--- front/src/routes.js
+++ front/src/routes.js
@@ -5,19 +5,19 @@ import Operador from './pages/Operador';
 import Login from './pages/Login';
 import CadastrarDemanda from './pages/CadastrarDemanda';
 import Equipamento from './pages/Equipamento';
+import Defeitos from './pages/Defeitos';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-    
-
         <Route exact path="/" component={Login} />
         <Route path="/login" component={Login} />
 
         <Route path="/operador/cadastrar" component={Operador} />
         <Route path="/demanda/cadastrar" component={CadastrarDemanda} />
         <Route path="/equipamento/relatorio" component={Equipamento} />
+        <Route path="/defeitos" component={Defeitos} />
       </Switch>
     </BrowserRouter>
   );
