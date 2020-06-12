ca68fc19972ebe884d73b2d40c64e55359bdf06b
Clara <43656831+claradbessa@users.noreply.github.com>
2020-06-11 23:03:56 -0300

    Cadastrar produção (backend)
    Backend do cadastro de produção

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
   connection: {
     host : '127.0.0.1',
     user : 'root',
-    password : 'root',
+    password : '',
     database : 'db'
   },
     migrations:{
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,7 @@ const OperadorController = require('./controllers/OperadorController');
 const DemandaController = require('./controllers/DemandaController');
 const ProdutoController = require('./controllers/ProdutoController');
 const LoginController = require('./controllers/LoginController');
+const ProducaoController = require('./controllers/ProducaoController');
 
 const routes = express.Router();
 
@@ -18,5 +19,8 @@ routes.get('/demandas',canAccess('1'), DemandaController.index);
 routes.post('/demandas', canAccess('1'), DemandaController.create);
 
 routes.get('/produtos', canAccess('1'), ProdutoController.index);
+
+routes.get('/producao', canAccess('1'), ProducaoController.index);
+routes.post('/producao', canAccess('1'), ProducaoController.create);
 
 module.exports = routes;
